ff4f8f9d23905eb9c6fea3ee22fb95b9f15c4084
Jeff Terry <terryj@iit.edu>
2020-12-17 15:41:15 -0600

    Initial Commit

---
+++ Shared/SwiftUICorePlotExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICorePlotExampleApp.swift
+//  Shared
+//
+//  Created by Jeff Terry on 12/17/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICorePlotExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
